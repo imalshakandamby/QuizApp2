0ef7e830a6e318ad3cb6e401b203c0c82fceaf0a
imalshakandamby <57284679+imalshakandamby@users.noreply.github.com>
2020-05-20 10:48:06 +0530

    Update quesAdded.php

--- quesAdded.php
+++ quesAdded.php
@@ -1,105 +1,54 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Add Question</title>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
-<?php
-    if(isset($_POST['submit'])){
-        $data_missing = array();
 
-        if(empty($_POST['question'])){
-            $data_missing[]='Question';
-        }else{
-            $ques = trim($POST['question']);
-        }
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
 
-        if(empty($_POST['ans1'])){
-            $data_missing[]='Answer1';
-        }else{
-            $ans1 = trim($POST['ans1']);
-            }
-
-        if(empty($_POST['ans2'])){
-           $data_missing[]='Answer2';
-        }else{
-            $ans2 = trim($POST['ans2']);
-        }
-
-        if(empty($_POST['ans3'])){
-            $data_missing[]='Answer3';
-        }else{
-            $ans3 = trim($POST['ans3']);
-        }
-
-        if(empty($_POST['ans4'])){
-            $data_missing[]='Answer4';
-        }else{
-            $ans4 = trim($POST['ans4']);
-        }
-
-        if(empty($data_missing)){
-            require_once('mysqli_connect.php');
-
-            $query = "INSERT INTO questions (question) VALUES(?) AND 
-            INSERT INTO answers (ans1,ans2, ans3, ans4) VALUES(?,?,?,?)";
-
-            $stmt = mysqli_prepare($dbc, $query);
-
-            //i Integer
-            //d Doubles
-            //b blobs
-            //s Everything Else
-
-            mysqli_stmt_bind_param($stmt, "sssss", $ques, $ans1, $ans2, $ans3, $ans4 );
-
-            mysqli_stmt_execute($stmt);
-
-            $affected_rows = mysqli_stmt_affected_rows($stmt);
-            if($affected_rows == 5){
-                    echo 'Question Entered';
-
-                    mysqli_stmt_close($stmt);
-                    mysqli_close($dbc);
-            }else{
-                echo 'Error Occurred<br />';
-                echo mysqli_error();
-
-                    mysqli_stmt_close($stmt);
-                    mysqli_close($dbc);
-            }
-        }else{
-            echo 'You need to enter the following data<br />';
-            foreach($data_missing as $missing){
-                echo "$missing<br />";
-            }
-        }
-    }
-?>
-
-<form action="quesAdded.php" method="post">
-
-<label>Question</label>
-<input type="text" name="question" size="250" value="">
-<br><br>
-
-<label>Answer 1</label>
-<input type="text" name="ans1" size="250" value="">
-<br>
-<label>Answer 2</label>
-<input type="text" name="ans2" size="250" value="">
-<br>
-<label>Answer 3</label>
-<input type="text" name="ans3" size="250" value="">
-<br>
-<label>Answer 4</label>
-<input type="text" name="ans4" size="250" value="">
+      <p>Welcome to Question page!</p>
+        <p><?php echo $_SESSION['question']; ?></p>
+        <br><br>
+        <p><?php echo $_SESSION['answer1']; ?></p>
+        <p><?php echo $_SESSION['answer2']; ?></p>
+        <p><?php echo $_SESSION['answer3']; ?></p>
+        <p><?php echo $_SESSION['answer4']; ?></p>
 
 
-
-<button type="submit" name="submit" value="Submit">Submit</button>
-
-</form>
-
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p> <a href="quesAdded.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+		
 </body>
 </html>
